f23b9b8e49509c01d68d52cdfbc4c288f4a8cee3
fgdaniel <mibercsk94@gmail.com>
2022-11-02 16:43:50 +0200

    Am sters dummy html si am adaugat utilizarea props

--- src/components/expense/Item.js
+++ src/components/expense/Item.js
@@ -1,17 +1,12 @@
 import "../../css/ExpenseItem.scss";
 
-function ExpenseItem() {
-	const date = new Date(2021, 2, 28);
-	const title = "Car insurance";
-	const amount = 294.67;
+function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
-			<div className="expense-item__date">
-				{date.toLocaleDateString()}
-			</div>
+			<div className="expense-item__date">{props.date}</div>
 			<div className="expense-item__content">
-				<h2 className="expense-item__title">{title}</h2>
-				<div className="expense-item__price">${amount}</div>
+				<h2 className="expense-item__title">{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
