e49567970b329ab50edca33447e0760868a90bdb
fgdaniel <mibercsk94@gmail.com>
2022-11-04 13:53:57 +0200

    Change date format of expenses

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import ExpenseItem from "./components/expense/Item";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
 	const expenses = [
 		{
 			title: "Car Insurance",
 			amount: 150,
-			date: new Date(2021, 2, 28).toLocaleDateString(),
+			date: new Date(2021, 2, 28),
 		},
 		{
 			title: "Health Insurance",
 			amount: 350,
-			date: new Date(2021, 5, 15).toLocaleDateString(),
+			date: new Date(2021, 5, 15),
 		},
 		{
 			title: "Taxes",
 			amount: 550,
-			date: new Date(2021, 8, 12).toLocaleDateString(),
+			date: new Date(2021, 8, 12),
 		},
 	];
 	return (
