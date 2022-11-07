4692d62f75281e5be7bb50c1cd524799db7c7c51
fgdaniel <mibercsk94@gmail.com>
2022-11-07 11:35:53 +0200

    Rearange html

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,11 +4,9 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
+			<div className="expense-item__price">${props.amount}</div>
+			<h2 className="expense-item__title">{props.title}</h2>
 			<ExpenseDate date={props.date} />
-			<div className="expense-item__content">
-				<h2 className="expense-item__title">{props.title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
-			</div>
 		</div>
 	);
 }
