a1ef478fde5e1dff847fda3d592887684ee974bb
fgdaniel <mibercsk94@gmail.com>
2022-11-07 12:27:01 +0200

    Style

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,7 +7,9 @@ function ExpenseDate(props) {
 	return (
 		<div className="expense-date">
 			<div className="expense-date__month">{month}</div>
+			<div className="expense-date__sep">/</div>
 			<div className="expense-date__day">{day}</div>
+			<div className="expense-date__sep">/</div>
 			<div className="expense-date__year">{year}</div>
 		</div>
 	);
