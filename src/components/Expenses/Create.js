05f7a6d3b1f62623a6c20fae10cf77119387bcfc
fgdaniel <mibercsk94@gmail.com>
2022-12-12 11:32:39 +0200

    Remove uneccesary code

--- src/components/Expenses/Create.js
+++ src/components/Expenses/Create.js
@@ -2,11 +2,8 @@ import ExpenseForm from "./Form";
 
 const ExpenseCreate = (props) => {
 	const saveDataHandler = (existingData) => {
-		const data = {
-			...existingData,
-			id: Math.random().toString(),
-		};
-		props.onAdd(data); // Calling prop as function
+		// console.log(existingData);
+		props.onAdd(existingData); // Calling prop as function
 	};
 	return (
 		<div>
