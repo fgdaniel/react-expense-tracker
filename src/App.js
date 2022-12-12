0ef1db257c6f2c7e6a59f801fc6391530ab4c5e1
fgdaniel <mibercsk94@gmail.com>
2022-12-12 11:33:25 +0200

    Update list to use state manager

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
+import { useState } from "react";
 import ExpenseCreate from "./components/Expenses/Create";
 import ExpenseList from "./components/Expenses/List";
 
+const dummy_expenses = [
+	{
+		title: "Car Insurance",
+		amount: 150,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		title: "Health Insurance",
+		amount: 350,
+		date: new Date(2020, 5, 15),
+	},
+	{
+		title: "Taxes",
+		amount: 550,
+		date: new Date(2019, 8, 12),
+	},
+];
+
 const App = () => {
-	const expenses = [
-		{
-			title: "Car Insurance",
-			amount: 150,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			title: "Health Insurance",
-			amount: 350,
-			date: new Date(2020, 5, 15),
-		},
-		{
-			title: "Taxes",
-			amount: 550,
-			date: new Date(2019, 8, 12),
-		},
-	];
+	const [getExpenses, setExpenses] = useState(dummy_expenses);
+
 	const addExpenseHandler = (expense) => {
-		console.log(expense);
+		setExpenses((prev) => {
+			return [expense, ...prev];
+		});
+		// console.log(expense);
 	};
 	return (
 		<div className="container space-y-6">
 			<ExpenseCreate onAdd={addExpenseHandler} />
-			<ExpenseList items={expenses} />
+			<ExpenseList items={getExpenses} />
 		</div>
 	);
 };
