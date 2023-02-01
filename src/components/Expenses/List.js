87da3af3183ed83a8dcf77bda8669f03f4c5b6d4
fgdaniel <mibercsk94@gmail.com>
2023-02-01 10:55:52 +0200

    Update html with conditionals

--- src/components/Expenses/List.js
+++ src/components/Expenses/List.js
@@ -3,22 +3,21 @@ import ExpenseItem from "./Item";
 import "./List.css";
 
 const ExpenseList = (props) => {
-	let expensesContent = (
-		<Card className="p-4 text-white">No expenses found</Card>
-	);
-
-	if (props.items.length > 0) {
-		expensesContent = props.items.map((item, index) => (
-			<ExpenseItem
-				key={index}
-				title={item.title}
-				amount={item.amount}
-				date={item.date}
-			/>
-		));
+	if (props.items.length === 0) {
+		return <Card className="p-4 text-white">No expenses found</Card>;
 	}
-
-	return <div className="expense-list">{expensesContent}</div>;
+	return (
+		<div className="expense-list">
+			{props.items.map((item, index) => (
+				<ExpenseItem
+					key={index}
+					title={item.title}
+					amount={item.amount}
+					date={item.date}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default ExpenseList;
